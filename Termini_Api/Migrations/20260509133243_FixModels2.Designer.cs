cb915b5593a33b3b218e2adb78c4d207226624e8
Djordje Antonovic <djordjeantonovic@gmail.com>
2026-05-09 15:33:57 +0200

    Add FixModels2 migration

--- Termini_Api/Migrations/20260509133243_FixModels2.Designer.cs
+++ Termini_Api/Migrations/20260509133243_FixModels2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Termini_Api.TerminiDbContext;
@@ -11,9 +12,11 @@ using Termini_Api.TerminiDbContext;
 namespace Termini_Api.Migrations
 {
     [DbContext(typeof(TerminiDBContext))]
-    partial class TerminiDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260509133243_FixModels2")]
+    partial class FixModels2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Termini_Api/Migrations/20260509133243_FixModels2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Termini_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixModels2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "OpenTo",
+                table: "Terens",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "OpenFrom",
+                table: "Terens",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OpenTo",
+                table: "Terens",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OpenFrom",
+                table: "Terens",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+        }
+    }
+}
